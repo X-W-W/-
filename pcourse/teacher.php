6f52ccc778350feda8099e24e854928e13b2b7db
X-W-W <35955572+X-W-W@users.noreply.github.com>
2018-12-29 02:52:39 +0800

    修改详细页的样式

---
+++ pcourse/teacher.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Bootstrap 实例 - 垂直的胶囊式导航菜单</title>
+		<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+		<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- Bootstrap core CSS -->
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="css/shop-item.css" rel="stylesheet">
+	</head>
+	<body>
+
+		<div class="tabbable">
+			<ul class="nav nav-pills">
+				<li class="active">
+					<a href="#tab1" data-toggle="tab">
+						张小健
+					</a>
+				</li>
+				<li>
+					<a href="#tab2" data-toggle="tab">
+						杨艳丽
+					</a>
+				</li>
+				<li>
+					<a href="#tab3" data-toggle="tab">
+						李延红
+					</a>
+				</li>
+				<li>
+					<a href="#tab4" data-toggle="tab">
+						朱映映
+					</a>
+				</li>
+				<li>
+					<a href="#tab5" data-toggle="tab">
+						林少聪
+					</a>
+				</li>
+				<li>
+					<a href="#tab6" data-toggle="tab">
+						蔡平
+					</a>
+				</li>
+				<li>
+					<a href="#tab7" data-toggle="tab">
+						薛丽萍
+					</a>
+				</li>
+			</ul>
+			<br>
+			<div class="tab-content">
+				<div class="tab-pane active " id="tab1">
+					<p>
+						强推强推强推~讲课讲得很好~又风趣幽默~上课当堂敲代码~能让你学到解题的思路~实验不算难~但有限时完成~
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						大学第一节课就爱上张小健！！！很有趣，有时上课会讲很多故事而且并不会让我觉得在浪费时间！！！不用ppt 上课认真听做笔记下课翻翻书so easy～总之超级喜欢他！！从来不点名！！去上课的都是真爱！！
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+					<hr>
+					<p>
+						放羊式教学，讲课有趣，通俗易懂，缺点只是实验课上指导会比较少，基本上交给师兄，个人认为这个无法怪他，总体还是挺喜欢的
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-10-01</small>
+				</div>
+				<div class="tab-pane" id="tab2">
+					<p>
+						讲课方式为ppt教学，效果因人而异
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						老师人很好，课堂氛围一般。
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+					<hr>
+					<p>
+						人很好，有点像妈妈的那种感觉。人很耐心。
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-10-01</small>
+				</div>
+				<div class="tab-pane" id="tab3">
+					<p>
+						老师很认真…但是我觉得讲课一般，有点无聊
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						教学内容详实 大作业需要全班演示
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+				</div>
+				<div class="tab-pane" id="tab4">
+					<p>
+						上课可能容易睡着，但是这个老师很好，考试容易得高分
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						机考咯 很可爱的老师 看我们上课死困死困就在第二节课播小短片给我们看 会体谅我们起不了床233点名也比较少 讲课讲得很详细 有耐心 课件也很赞
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+					<hr>
+					<p>
+						本身一门专业性较强的课程，但是她经常会上课讲点幽默笑话活跃下气氛，上课调理清晰，易懂，挺关心同学的。平时的实验作业较多，但是也还是为了你们期末能考好嘿嘿
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-10-01</small>
+				</div>
+				<div class="tab-pane" id="tab5">
+					<p>
+						好，上机课做练习，对期末特别有帮助。其他班上机课自习，期末没底气。
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						只有实验课点过两次名，人超级好，讲的也很明白，建议选她
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+					<hr>
+					<p>
+						虽然说上课没有其他一些老师好，但是不点名的风格让我有了个很闲的第一学期而且没有课上作业哦，亲，放心大胆的翘了吧……………那么你不就有时间去蹭其他老师的课了吗？！！
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-10-01</small>
+				</div>
+				<div class="tab-pane" id="tab6">
+					<p>
+						很有耐心的老师，讲课速度快质量却很好
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+					<hr>
+					<p>
+						计算机零基础同学福音！如果你是计算机系，并且没有基础，强烈建议选蔡平，虽然她上课也满口专业术语，到最主要的是她会边编程边讲解，即使听不懂，看也能看懂了吧！所以如果没有基础，建议选她的课
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-11-11</small>
+					<hr>
+					<p>
+						老师给人的感觉很踏实，很厚道，很关心学生真的……很好，下课时间跟我们说了很多课外的东西，下学期我还选她的课！
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-10-01</small>
+				</div>
+				<div class="tab-pane" id="tab7">
+					<p>
+						上课的话氛围还是比较放松的，老师讲的也很详细。没作业，要说有的话就是上机的时候没敲完的题课下要敲完。结合慕课，用慕课课前预习和复习，上课的时候一定能听懂，上机的时候认真点敲，最后会发现进步很大的
+					</p>
+					<small class="text-muted">Posted by xxx on 2018-12-25</small>
+				</div>
+			</div>
+		</div>
+		<script src="vendor/jquery/jquery.min.js"></script>
+    	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
